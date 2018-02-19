ae6544de3b1f0e874159d99eb300dd3c9c2e99da
Hoon Lee <yl.hoony@gmail.com>
2018-02-19 09:22:54 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,29 @@ class SessionsController < ApplicationController
 
   def create
     # raise params.inspect
-    binding.pry
+    # binding.pry
+
+    # {"provider"=>"facebook",
+    #  "uid"=>"10156760494199749",
+    #  "info"=>
+    #   {"email"=>"dreamaire@hotmail.com",
+    #    "name"=>"Hoon Lee",
+    #    "image"=>"http://graph.facebook.com/v2.6/10156760494199749/picture"},
+    #  "credentials"=>
+    #   {"token"=>
+    #     "EAACR09ljRBIBAGcGfI8AqtmHZC648rNe1cl8kQkFQqBCauxvaEduZACZAx1mOAl8XW1ZCUXlcywZAgerUX4aHJZBVZ
+    # MaiZCiZA22ZBAzHeFjeE9LchocNhZAJotUjE9w3RgBGAZCutHq5CSnReqpmfCjzjsXTDRIZCQZDZD",
+    #    "expires_at"=>1524188299,
+    #    "expires"=>true},
+    #  "extra"=>
+    #   {"raw_info"=>
+    #     {"name"=>"Hoon Lee",
+    #      "email"=>"dreamaire@hotmail.com",
+    #      "id"=>"10156760494199749"}}}
+
+    @user = User.find_or_create_by(uid: auth[:uid])
+
+    
 
 
 
