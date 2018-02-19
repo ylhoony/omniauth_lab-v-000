844c633d675b6749159c766b31c4214bf14eb109
Hoon Lee <yl.hoony@gmail.com>
2018-02-19 08:47:54 +0000

    automatically backed up by learn

--- spec/views/home.html.erb_spec.rb
+++ spec/views/home.html.erb_spec.rb
@@ -4,7 +4,6 @@ require 'pry'
 
 describe "welcome/home.html.erb" do
   it "displays all the widgets" do
-    binding.pry
     render
     expect(rendered).to have_link('Login with Facebook', :href => '/auth/facebook')
   end
