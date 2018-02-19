f98c938ae33066337f740458336611bbac851504
Hoon Lee <yl.hoony@gmail.com>
2018-02-19 08:43:12 +0000

    automatically backed up by learn

--- spec/views/home.html.erb_spec.rb
+++ spec/views/home.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe "welcome/home.html.erb" do
-  it "displays all the widgets" do
+  it "displays all the widgets", type: :view do
     render
     expect(rendered).to have_link('Login with Facebook', :href => '/auth/facebook')
   end
